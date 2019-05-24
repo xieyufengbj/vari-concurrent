5fb5edab3d52b71b8a9cb9eab7fdf636ac2de8c3
XieYufeng <xieyufeng@mimidai.com>
2019-05-24 16:40:48 +0800

    test

--- src/main/java/com/variflight/ch01/RunAndStart.java
+++ src/main/java/com/variflight/ch01/RunAndStart.java
@@ -8,9 +8,23 @@ package com.variflight.ch01;
  */
 public class RunAndStart {
 
-
+    public static class RunThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("["+ Thread.currentThread().getId() +"], 线程名：" + Thread.currentThread().getName());
+        }
+    }
 
     public static void main(String[] args) {
-
+        Thread thread = new RunThread();
+        // 相当于直接调用普通对象的方法
+        //thread.run();
+        // 调用start，虚拟机会将该方法映射到操作系统中线程
+        thread.start();
     }
 }
